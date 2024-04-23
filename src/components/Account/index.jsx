b83000b0efd0c793e8f9af4197408fd606c08286
Yuliia Hots <goculiya@gmail.com>
2024-04-23 14:22:19 +0200

    L'ajout de la composant account

---
+++ src/components/Account/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./account.css";
+
+function Account(children) {
+  return (
+    <div className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{children.title}</h3>
+        <p className="account-amount">{children.amount}</p>
+        <p className="account-amount-description">{children.text}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </div>
+  );
+}
+
+export default Account;
