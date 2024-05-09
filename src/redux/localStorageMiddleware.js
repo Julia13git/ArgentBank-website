18a57ec076a84e51ecd7257181ad5fce8b2db771
Yuliia Hots <goculiya@gmail.com>
2024-05-09 11:06:41 +0200

    La suppression du fichier inoutil

--- src/redux/localStorageMiddleware.js
+++
@@ -1,7 +0,0 @@
-export const localStorageMiddleware = store => next => action => {
-    const result = next(action);
-  
-    // Save the Redux state to localStorage after every action
-    localStorage.setItem('reduxState', JSON.stringify(store.getState()));
-    return result;
-  };
